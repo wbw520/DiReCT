b872d11faafd2088b5ecd2c84507b9b4b392acc5
wbw520 <wangbowenjisuanji@yahoo.co.jp>
2024-06-13 13:08:27 +0900

    revise

--- utils/gpt_call.py
+++ utils/gpt_call.py
@@ -2,8 +2,8 @@ from openai import AzureOpenAI, OpenAI
 import os
 
 
-def AskChatGPT(input_template, temperature, model_engine):
-    os.environ['OPENAI_API_KEY'] = 'your keu'
+def AskChatGPT(input_template, temperature, model_engine, api_key):
+    os.environ['OPENAI_API_KEY'] = api_key
     client = OpenAI(
         # This is the default and can be omitted
         api_key=os.environ.get("OPENAI_API_KEY"),
@@ -24,9 +24,9 @@ def AskChatGPT(input_template, temperature, model_engine):
     return response
 
 
-def AskGPTCompany(input_template, temperature):
+def AskGPTCompany(input_template, temperature, api_key):
     # #gpt3.5
-    # client_gpt=AzureOpenAI(api_key="d8e8fc52cbfe4495adc605fe412ce3eb",
+    # client_gpt=AzureOpenAI(api_key=api_key,
     #                    azure_endpoint = "https://cog-42bbrearckery.openai.azure.com/",
     #                   api_version="2023-05-15")
     #
@@ -42,26 +42,9 @@ def AskGPTCompany(input_template, temperature):
     #     top_p=1,
     #     stop=None)
 
-    # #gpt3.5
-    # client_gpt=AzureOpenAI(api_key="830013c9e8d741829925f81e5efcdc6a",
-    #                    azure_endpoint = "https://wbw520520.openai.azure.com/",
-    #                   api_version="2024-02-15-preview")
-    #
-    # response = client_gpt.chat.completions.create(
-    #     model="gpt-35-turbo-16k",  # for gpt3.5 use chat, gpt4 use gpt4 as model
-    #     messages=[
-    #             {
-    #                 "role": "user",
-    #                 "content": input_template,
-    #             }
-    #         ],
-    #     temperature=temperature,
-    #     top_p=1,
-    #     stop=None)
-
 
     #gpt4
-    client_gpt4 = AzureOpenAI(api_key="830013c9e8d741829925f81e5efcdc6a",
+    client_gpt4 = AzureOpenAI(api_key=api_key,
                        azure_endpoint = "https://wbw520520.openai.azure.com/",
                       api_version="2024-02-15-preview")
 

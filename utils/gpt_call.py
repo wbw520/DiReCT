34c14da6bf57f6c8ad9c4a66a9ff6c7275606709
wbw520 <wangbowenjisuanji@yahoo.co.jp>
2024-06-13 13:19:40 +0900

    revise

--- utils/gpt_call.py
+++ utils/gpt_call.py
@@ -2,7 +2,7 @@ from openai import AzureOpenAI, OpenAI
 import os
 
 
-def AskChatGPT(input_template, temperature, model_engine, api_key):
+def AskChatGPT(input_template, model, api_key, temperature=0, top_p=1):
     os.environ['OPENAI_API_KEY'] = api_key
     client = OpenAI(
         # This is the default and can be omitted
@@ -16,40 +16,21 @@ def AskChatGPT(input_template, temperature, model_engine, api_key):
                 "content": input_template,
             }
         ],
-        model=model_engine,
+        model=model,
         temperature=temperature,
-        top_p=1,
+        top_p=top_p,
         stop=None
     )
     return response
 
 
-def AskGPTCompany(input_template, temperature, api_key):
-    # #gpt3.5
-    # client_gpt=AzureOpenAI(api_key=api_key,
-    #                    azure_endpoint = "https://cog-42bbrearckery.openai.azure.com/",
-    #                   api_version="2023-05-15")
-    #
-    # response = client_gpt.chat.completions.create(
-    #     model="chat",  # for gpt3.5 use chat, gpt4 use gpt4 as model
-    #     messages=[
-    #             {
-    #                 "role": "user",
-    #                 "content": input_template,
-    #             }
-    #         ],
-    #     temperature=temperature,
-    #     top_p=1,
-    #     stop=None)
+def AskGPTAzure(input_template, api_key, azure_endpoint, api_version, model, temperature=0, top_p=1):
+    client = AzureOpenAI(api_key=api_key,
+                       azure_endpoint=azure_endpoint,
+                      api_version=api_version)
 
-
-    #gpt4
-    client_gpt4 = AzureOpenAI(api_key=api_key,
-                       azure_endpoint = "https://wbw520520.openai.azure.com/",
-                      api_version="2024-02-15-preview")
-
-    response = client_gpt4.chat.completions.create(
-        model="gpt-4-32k",
+    response = client.chat.completions.create(
+        model=model,
         messages=[
             {
                 "role": "user",
@@ -57,14 +38,7 @@ def AskGPTCompany(input_template, temperature, api_key):
             }
         ],
         temperature=temperature,
-        top_p=1,
+        top_p=top_p,
         stop=None)
 
     return response
-
-#
-# tempp = "Please tell me the name of apple's current CEO."
-#
-# # res = AskGPTCompany(tempp, temperature=0)
-# res = AskChatGPT(tempp, temperature=0, model_engine="gpt-4")
-# print(res.choices[0].message.content)

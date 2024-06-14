7e98c7cbcf552e71b3a237e28412bb589c813567
wbw520 <wangbowenjisuanji@yahoo.co.jp>
2024-06-14 16:21:09 +0900

    revise

--- evaluation.py
+++ evaluation.py
@@ -1,6 +1,3 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed in accordance with the terms of the Llama 3 Community License Agreement.
-
 from typing import List, Optional
 from utils.data_analysis import deduction_assemble
 from utils.data_extraction import discriminate_similarity_observation, discriminate_similarity_reason
